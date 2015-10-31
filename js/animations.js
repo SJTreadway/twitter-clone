58ec8dd1fe9a6ef9576b0aef6685b91b62a5ee0f
SJTreadway <treadway08@gmail.com>
2015-10-31 18:21:57 -0500

    completes twitter clone

--- js/animations.js
+++ js/animations.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 	$('.tweet-compose').on('click', function() {
 		$('#tweet-controls').show();
 		$(this).css('height', '5em');
@@ -22,12 +23,13 @@ $(document).ready(function() {
 	});				
 
 	$('#tweet-submit').on('click', function() {
-		$('#stream').prepend(function() {
-			var newTweetImg = "<img class='avatar' src='img/alagoon.jpg' />";
-			var newTweetFullName = "<strong class='fullname'>Steven Treadway</strong>";
-			return newTweetImg + newTweetFullName;
-		});
+		var newTweet = $('.tweet-compose').val();
+		$('#stream').prepend("<div id='stream'><div class='tweet'><div class='content'><img class='avatar' src='img/alagoon.jpg' /><strong class='fullname'>Steven Treadway</strong><span class='username'>@sjt</span><p class='tweet-text'>" + newTweet + "</p><div class='tweet-actions'><ul><li><span class='icon action-reply'></span> Reply</li><li><span class='icon action-retweet'></span> Retweet</li><li><span class='icon action-favorite'></span> Favorite</li><li><span class='icon action-more'></span> More</li></ul></div><div class='stats'><div class='retweets'><p class='num-retweets'>30</p><p>RETWEETS</p></div><div class='favorites'><p class='num-favorites'>6</p><p>FAVORITES</p></div><div class='users-interact'><div><img src='img/alagoon.jpg' /><img src='img/vklimenko.jpg' /></div></div><div class='time'>1:04 PM - 19 Sep 13</div></div><div class='reply'><img class='avatar' src='img/alagoon.jpg' /><textarea class='tweet-compose' placeholder='Reply to @sjt'/></textarea></div></div></div>");
+		newTweet = $('.tweet-compose').val('');
 	});
 
+	$('.tweet').on('click', function() {
+		$(this).find('.stats').show();
+	});
 
 });
